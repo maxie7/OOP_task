a0fdce24d3ed68ce96efecf0bd4331b263b1122b
Max Kos <maxie7@protonmail.com>
2016-05-05 01:47:27 +0300

    Everything that is simple sometimes is Perfect

--- library.rb
+++ library.rb
@@ -1,69 +1,34 @@
 require_relative './structure.rb'
+require_relative './db'
 
-class Library < Struct.new(:books, :orders, :readers, :authors)
-
+class Library
   include Enumerable
 
-  # Reading and converting info from data files
-  @books = File.readlines("data/books")
-  @books.each_index { |x| @books[x] = Book.new(@books[x], @books[x+1]) if x % 2 == 0 }
-  @books.delete_if { |e| e.is_a?(String) }
+  attr_accessor :books, :orders, :readers, :authors
 
-  @authors = Array.new
-  @auth = File.readlines("data/authors")
-  @auth.each { |line|
-    words = line.split(';')
-    a = Author.new
-    a.name = words[0].strip
-    a.biography = words[1].strip
-    @authors.push(a)
-  }
+  def initialize books, orders, readers, authors
+    @authors = authors
+    @books = books
+    @readers = readers
+    @orders = orders
+  end
 
-  @readers = Array.new
-  @read = File.readlines("data/readers")
-  @read.each { |line|
-    words = line.split(',')
-    r = Reader.new
-    r.name = words[0].strip
-    r.email = words[1].strip
-    r.city = words[2].strip
-    r.street = words[3].strip
-    r.house = words[4].strip
-    @readers.push(r)
-  }
-
-  @@orders = Array.new
-  @ord = File.readlines("data/orders")
-  @ord.each { |line|
-    words = line.split(',')
-    o = Order.new
-    o.book = words[0].strip
-    o.reader = words[1].strip
-    o.date = words[2].strip
-    @@orders.push(o)
-  }
-
-  # who often takes the book
+  #who often takes the book
   def who_ottb
-    @_orders = @@orders.group_by {|x| x.reader }
-    @often_reader = @_orders.keys.take(1)
-    puts @often_reader
+    often_reader = @orders.group_by{|x| x.reader}.sort_by {|x,y| y.length}.last[0].name
   end
   # the most popular book
   def the_most_pb
-    @_orders = @@orders.group_by {|x| x.book }
-    @popular_book = @_orders.keys.take(1)
-    puts @popular_book
+    popular_book = @orders.group_by{|x| x.book}.sort_by{|x,y| y.length}.last[0].title
   end
 
   # How many people ordered one of the three most popular books
   def how_many_pooottmpb
-    @order_books = []
-    @count_people = @@orders.each_index do |element|
-      @order_books << @@orders[element].book
+    @ordered_books = []
+    @orders.each_index do |element|
+      @ordered_books << @orders[element].book.title
     end
-    @people = @order_books.uniq.inject({}) { |a, e| a.merge( {e => @order_books.count(e)} ) }
-    puts @people.sort_by{|key, value| value}.reverse.take(3)
+    @people = @ordered_books.uniq.inject({}) {|a, e| a.merge( {e => @ordered_books.count(e)} ) }
+    return @people.sort_by{|key, value| value}.reverse.take(3)
   end
-
 end
--- oop-task.rb
+++ oop-task.rb
@@ -6,24 +6,18 @@ puts "Openning file..."
     library = Marshal.load(File.binread('result.sav'))
   rescue
     puts 'Failed.'
-    library = Library.new
+    library = db
     puts "Loaded default."
   else
     puts "Ok."
   end
 
 puts
-
-puts "Reader who often takes the books: "
-library.who_ottb
+puts "Reader who often takes the books: #{library.who_ottb}"
 puts
-
-puts "The most popular book: "
-library.the_most_pb
+puts "The most popular book: \"#{library.the_most_pb}\""
 puts
-
-puts "How many people who ordered one of the three popular books: "
-library.how_many_pooottmpb
+puts "How many people who ordered one of the three popular books: \n #{library.how_many_pooottmpb}"
 puts
 
 puts "All data has been saved to the file!"
